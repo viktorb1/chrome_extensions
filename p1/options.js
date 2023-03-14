f48a07f0b79815408743d8b499c15e2e1dcd8549
Viktor Babchanik <vbabchanik@ucdavis.edu>
2023-03-14 15:39:41 -0700

    Add p1

---
+++ p1/options.js
@@ -0,0 +1,19 @@
+console.log("hello from the options page!");
+
+const nameInput = document.getElementById("name-input");
+const timeInput = document.getElementById("time-input");
+const saveBtn = document.getElementById("save-btn");
+
+saveBtn.addEventListener("click", () => {
+  const name = nameInput.value;
+  const notificationTime = timeInput.value;
+  chrome.storage.sync.set({
+    name,
+    notificationTime,
+  });
+});
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
+  nameInput.value = res.name ?? "???";
+  timeInput.value = res.notificationTime ?? 1000;
+});
